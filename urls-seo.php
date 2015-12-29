f81bc436b11f73fc7039a125d4355d105bfb1fb9
Vu Tran <phongvu.811@gmail.com>
2015-12-29 10:59:06 +0700

    Update 29-12-2015

---
+++ urls-seo.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ Plugin Name: Urls SEO
+ Plugin URI: http://wordpress.org/plugins/urls-seo/
+ Description: This plugin can make your permalinks pretty (Go to Plugins -> Urls SEO).
+ Version: 1.0
+ Author: Vu Tran
+ Author URI: http://vutran.vn
+ Text Domain: urls-seo
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
